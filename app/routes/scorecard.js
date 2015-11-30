885d5b26ea0363a8f73d669785d143ee77f63c35
Ubuntu <timothy.dewees@gmail.com>
2015-11-30 13:22:19 +0000

    Stubbed out other methods

--- app/routes/scorecard.js
+++ app/routes/scorecard.js
@@ -74,6 +74,7 @@ module.exports = function(router) {
     }
   });
 
+  // [/scorecard] route, returns list of all scorecards for an admin app and allows POST to create new
   router.route('/')
   .get(function(req, res, next) {
 
@@ -85,10 +86,175 @@ module.exports = function(router) {
 
   }).post(function(req, res, next) {
      
-    var card = new scorecard({user:{email:"whtdrgn101@gmail.com"}});
-    card.save(function(err, cd){
-      res.json(cd);
-    });
+    res.sendStatus(201);
    
+  });
+  
+  // [/scorecard/:userId/bows] - Returns list of bows for provided users
+  router.route('/:userid/bows')
+  .get(function(req, res, next) {
+
+    scorecard.findOne({'user.userId': req.params.userId}).select('bows').exec(function (err, bows) {
+      if (err) { throw err; }
+
+      res.send(bows);
+    });
+
+  }).post(function(req, res, next) {
+     
+    res.sendStatus(201);
+   
+  });
+  
+  // [/scorecard/:userId/bows/:bowNum] - Work with a specific bow
+  router.route('/:userId/bows/:bowNum')
+  .get(function(req, res, next) {
+    if (req.params.userId) {
+      scorecard.findOne({'user.userId': req.params.userId, 'bows.bowNum': req.params.bowNum}).select('bows').exec(function (err, bow) {
+        if (err) {
+          throw err;
+        }
+        if(bow) {
+          res.send(bow);
+        } else {
+          res.sendStatus(404);
+        }
+
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  }) 
+  .put(function(req, res, next) {
+    if (req.params.userId) {
+      scorecard.findOne({'user.userId': req.params.userId}, function (err, card) {
+        if (err) {
+          throw err;
+        }
+
+        if(card) {
+          card.update(req.body, function(err, c){
+            if (err) {
+              throw err;
+            }
+            card.save();
+            res.sendStatus(202);
+          });
+        } else {
+          res.sendStatus(404);
+        }
+
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  })
+  .patch(function(req, res,next) {
+    if (req.params.userId) {
+      scorecard.findOne({'user.userId': req.params.userId}, function (err, scorecards) {
+        if (err) {
+          throw err;
+        }
+        //TODO: Replace record
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  })
+  .delete(function(req, res, next) {
+    if (req.params.userId) {
+      scorecard.findOne({'user.userId': req.params.userId}).remove(function (err, scorecards) {
+        if (err) {
+          throw err;
+        }
+        res.sendStatus(202);
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  });
+  
+  // [/scorecard/:userId/rounds] - Returns list of rounds for provided users
+  router.route('/:userid/rounds')
+  .get(function(req, res, next) {
+
+    scorecard.findOne({'user.userId': req.params.userId}).select('rounds').exec(function (err, rounds) {
+      if (err) { throw err; }
+
+      res.send(rounds);
+    });
+
+  }).post(function(req, res, next) {
+     
+    res.sendStatus(201);
+   
+  });
+  
+  // [/scorecard/:userId/rounds/:roundNum] - Work with a specific round
+  router.route('/:userId/bows/:bowNum')
+  .get(function(req, res, next) {
+    if (req.params.userId) {
+      scorecard.findOne({'user.userId': req.params.userId, 'rounds.roundNum': req.params.bowNum}).select('rounds').exec(function (err, round) {
+        if (err) {
+          throw err;
+        }
+        if(round) {
+          res.send(round);
+        } else {
+          res.sendStatus(404);
+        }
+
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  }) 
+  .put(function(req, res, next) {
+    if (req.params.userId) {
+      scorecard.findOne({'user.userId': req.params.userId}, function (err, card) {
+        if (err) {
+          throw err;
+        }
+
+        if(card) {
+          card.update(req.body, function(err, c){
+            if (err) {
+              throw err;
+            }
+            card.save();
+            res.sendStatus(202);
+          });
+        } else {
+          res.sendStatus(404);
+        }
+
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  })
+  .patch(function(req, res,next) {
+    if (req.params.userId) {
+      scorecard.findOne({'user.userId': req.params.userId}, function (err, scorecards) {
+        if (err) {
+          throw err;
+        }
+        //TODO: Replace record
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  })
+  .delete(function(req, res, next) {
+    if (req.params.userId) {
+      scorecard.findOne({'user.userId': req.params.userId}).remove(function (err, scorecards) {
+        if (err) {
+          throw err;
+        }
+        res.sendStatus(202);
+      });
+    } else {
+      res.sendStatus(404);
+    }
   });
 };
