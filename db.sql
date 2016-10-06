cc6dc5024f484b0aa5466438346d7292cb0b467a
Ubuntu <timothy.dewees@gmail.com>
2016-10-06 13:53:51 +0000

    Updating member record

--- db.sql
+++ db.sql
@@ -15,6 +15,8 @@ CREATE TABLE bow_type (
 CREATE TABLE member (
 	id INT not null auto_increment
 	,email VARCHAR(255) NOT NULL
+	,name VARCHAR(255) NULL
+	,profile_pic BLOB NULL
 	,active BOOLEAN NOT NULL DEFAULT true
 	,password VARCHAR(255) NOT NULL
 	, primary key (id)
